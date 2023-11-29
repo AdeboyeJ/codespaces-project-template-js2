3d05f549d2b423d3095c9877dd4d365d32900c2e
Adeshina Joshua <74295466+AdeboyeJ@users.noreply.github.com>
2023-11-29 14:18:52 +0000

    main commit

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,14 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Joshua Adeshina",
+  title: "UI/UX Designer",
+  email: "adeshinajoshua4@gmail.com",
+  gitHub: "AdeboyeJ",
+  instagram: "AdeboyeJ",
+  linkedIn: "Adeshina Joshua",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "AdeboyeJ",
 };
 
 const primaryColor = "#4E567E";
